aa67f6bf75f6cf6d48c7f37c67c0fdb9fb96e40f
Michelle Oh <ohmotrl@gmail.com>
2024-11-13 13:41:39 -0500

    updated functions

--- main.go
+++ main.go
@@ -14,27 +14,9 @@ func main() {
 	greetUsers(conferenceName, conferenceTickets, remainingTickets)
 
 	for {
-		var firstName string
-		var lastName string
-		var email string
-		var userTickets uint
-		// User input
-		fmt.Println("Enter your first name: ")
-		fmt.Scan(&firstName) // & is a pointer that will print out the memory address of the variable
+		firstName, lastName, email, userTickets := getUserInput()
 
-		fmt.Println("Enter your last name: ")
-		fmt.Scan(&lastName)
-
-		fmt.Println("Enter your email address: ")
-		fmt.Scan(&email)
-
-		fmt.Println("Enter number of tickets: ")
-		fmt.Scan(&userTickets)
-
-		// User input validation
-		var isValidName bool = len(firstName) >= 2 && len(lastName) >= 2
-		isValidEmail := strings.Contains(email, "@")
-		isValidTicketCount := userTickets > 0 && userTickets <= remainingTickets
+		isValidName, isValidEmail, isValidTicketCount := validateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 
 		if isValidName && isValidEmail && isValidTicketCount { // if all 3 are true
 			remainingTickets = remainingTickets - userTickets
@@ -44,11 +26,7 @@ func main() {
 			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
 			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-			firstNames := []string{}
-			for _, element := range bookings { // underscore is a blank identifier to ignore a variable you don't want to use
-				var names = strings.Fields(element)
-				firstNames = append(firstNames, names[0])
-			}
+			firstNames := getFirstNames(bookings)
 			fmt.Printf("The first names of bookings: %v\n", firstNames)
 
 			if remainingTickets == 0 {
@@ -73,4 +51,42 @@ func greetUsers(confName string, confTickets int, remainingTickets uint) { // ex
 	fmt.Printf("Welcome to %v booking application!\n", confName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", confTickets, remainingTickets)
 	fmt.Println("Get your tickets here")
+}
+
+func getFirstNames(bookings []string) []string {
+	firstNames := []string{}
+	for _, element := range bookings { // underscore is a blank identifier to ignore a variable you don't want to use
+		var names = strings.Fields(element)
+		firstNames = append(firstNames, names[0])
+	}
+	return firstNames
+}
+
+func validateUserInput(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
+	var isValidName bool = len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTicketCount := userTickets > 0 && userTickets <= remainingTickets
+	// In Go, you can return multiple values
+	return isValidName, isValidEmail, isValidTicketCount
+}
+
+func getUserInput() (string, string, string, uint) {
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName) // & is a pointer that will print out the memory address of the variable
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	return firstName, lastName, email, userTickets
 }
